5d5b024c67189f1b2f22219a08dbeb8a9530a12d
karri neha sri <karrinehasri@karris-MacBook-Air-8.local>
2026-04-13 15:43:58 +0530

    UC1 to UC4 Complete

---
+++ UC3.java
@@ -0,0 +1,9 @@
+public class UC3 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Please provide a name as a command-line argument.");
+        }
+    }
+}
